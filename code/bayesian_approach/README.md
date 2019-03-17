363416f52f8abdb42c88d43ca01c21fffab2f191
DSM <dan.s.maynard@gmail.com>
2019-03-17 16:44:22 -0500

    created markdown for bayes fit

---
+++ code/bayesian_approach/README.md
@@ -0,0 +1,200 @@
+Fitting endpoints: Bayesian approach
+================
+Daniel Maynard, Zachary Miller and Stefano Allesina
+
+Here we show how to fit the endpoints or predict out of fit using the Bayesian approach, as presented in the main text. This method accounts for log-normal measurement error in each endpoint abundance, and for the fact that each endpoint appears in multiple different equations. This method is the recommended appraoch for fitting this model, though it is computationally intensive.
+
+All the code needed for the analysis is in the file `bayes.R`. The Bayesian MCMC algorithm is implemented in the Stan programming language, which is called here from within `R`. The details of model is given in `Stan_file.stan`, which instructs the MCMC samler how to calculate the log-likelihood at each step. Please see `https://mc-stan.org/users/interfaces/` for isntructions on installing Stan. In addition to Stan, the libraries `rstan`, `tidyverse`, and `coda` need to be installed for this code to run.
+
+``` r
+source("bayes.R")
+```
+
+### Step 1: label the data
+
+First, call the function prepare\_data using one of the available data sets, or any other data set organized in the same manner. The data file should be in csv format, with one column for each species, and one row for each recorded endpoint. The header should specify the name of the species. For example:
+
+``` r
+dt <- read_csv("../../data/Kuebbing_plants/natives.csv")
+dt %>% sample_n(10) # show 10 endpoints sampled at random
+```
+
+    ## # A tibble: 10 x 4
+    ##       as    fa    la    po
+    ##    <dbl> <dbl> <dbl> <dbl>
+    ##  1 0      2.52 0     2.56 
+    ##  2 1.47   0    0     0    
+    ##  3 0      0    0     4.03 
+    ##  4 0.487  0    0     3.07 
+    ##  5 0      0    1.01  0    
+    ##  6 2.36   0    0     0    
+    ##  7 0      0    0     7.29 
+    ##  8 2.01   3.55 0.495 0    
+    ##  9 0.901  0    0.183 2.66 
+    ## 10 1.00   2.43 0     0.986
+
+The function prepare\_data simply adds a column containing a label for the community. For example:
+
+``` r
+dt <- prepare_data("~/Git/endpoints/github/data/Kuebbing_plants/invasives.csv")
+dt %>% sample_n(10)
+```
+
+    ## # A tibble: 10 x 5
+    ##       as    fa    la    po community  
+    ##    <dbl> <dbl> <dbl> <dbl> <chr>      
+    ##  1  0    11.7  0     0     fa         
+    ##  2  0     5.18 1.17  0.899 fa-la-po   
+    ##  3  1.99  0    0     1.04  as-po      
+    ##  4  0     5.56 1.56  0     fa-la      
+    ##  5  0     0    2.10  0     la         
+    ##  6  0     0    2.29  0.498 la-po      
+    ##  7  0     6.85 0     1.43  fa-po      
+    ##  8  0     4.84 0.902 1.05  fa-la-po   
+    ##  9  2.24  0    0     0     as         
+    ## 10  1.94  3.55 0.420 0.696 as-fa-la-po
+
+### Step 2: fitting all endpoints
+
+To use all of the available data to estiamte *B* via a Bayesian approach, call the function `fit_stan`, which calls the `stan` function within R. The `fit_stan` function allows you to adjust several parameters:
+
+-   `stan_file` gives the location of the Stan file used for fittnig
+-   `exclude` speficies which communities to exclude. If set to `NULL` (default) all endpoints are used. Otherwise it takes a character vector of communities, e.g., `c("fa","fa-po")`.
+-   `B_upper` is a binary matrix of dimension *n* × *n* indicating the upper bounds on each entry of B. A matrix of all 1s would specify no upper bound for any element; a matrix with 1s on the off-diagonal and 0 on the diagonal would constrain the diagonal entries to be negative. The default is a purely competitive community (`B_upper = 0`)
+-   `B_lower` is a binary matrix of dimension *n* × *n* indicating the lower bounds on each entry of B. A matrix of all −1s would specify no lower bound for any element; a matrix with −1s on the off-diagonal and 0 on the diagonal would constrain the diagonal entries to be positive. The defauls is no lower bound (`B_upper = -1`)
+-   `chains` the number of mcmc chains to run
+-   `cores` the number of processor cores to use
+-   `iter` the number of MCMC iterations
+-   `warmup` the number of warmup iterations before sampling
+-   `thin` the number of iterations to skip between samples
+-   `seed` the random seem for the Stan function (default is `10`)
+
+For example, here we fit two short chains (5000 iterations) to the full invasive plant data using two cores:
+
+``` r
+stan_results <- fit_stan(dt, stan_file = "Stan_file.stan", chains = 2, 
+                   cores = 2, iter = 5000, warmup = 2000, thin = 15)
+```
+
+    ## [1] "fitting all endpoints"
+
+Plotting diagnostics
+--------------------
+
+The resulting output is a named list that contains the details of the fit (original data, exluded communities, number of iterations, etc.) as well as the MCMC results. For example, here are the first 10 iterations of the MCMC run, showing the first 4 coefficients (first column) of *B*:
+
+``` r
+as.matrix(stan_results$stan_fit)[1:10,1:4]
+```
+
+    ##           parameters
+    ## iterations     B[1,1]     B[2,1]     B[3,1]     B[4,1]
+    ##       [1,] -0.4538348 -0.2835567 -0.3201802 -0.3294600
+    ##       [2,] -0.4465964 -0.2924835 -0.3275420 -0.2992859
+    ##       [3,] -0.4260008 -0.2662578 -0.3090865 -0.2884263
+    ##       [4,] -0.4493458 -0.2865314 -0.3308038 -0.3176141
+    ##       [5,] -0.4730513 -0.2838069 -0.3370264 -0.3344845
+    ##       [6,] -0.4928515 -0.2562541 -0.3324371 -0.3159799
+    ##       [7,] -0.5264155 -0.3314866 -0.3587454 -0.3370532
+    ##       [8,] -0.5246959 -0.2860623 -0.3250148 -0.2951149
+    ##       [9,] -0.4743201 -0.2891395 -0.3511693 -0.3337661
+    ##      [10,] -0.4513145 -0.2758522 -0.3158761 -0.3125297
+
+From this output, we can plot diagnostics by calling the `plot_diagnostics` function, specifying either histogram of *B* (`show_plot = "hist"`) or the full MCMC output across all iterations (`show_plot = "chains"`), or both. For example, here's the histogram of the posterior of *B*:
+
+``` r
+plot_diagnostics(stan_results, show_plot = "hist") 
+```
+
+    ## [1] "Plotting histogram of B"
+
+    ## `stat_bin()` using `bins = 30`. Pick better value with `binwidth`.
+
+![](bayes_files/figure-markdown_github/unnamed-chunk-6-1.png)
+
+### Step 3: predicting endpoints
+
+To predict the endpoint abundance for a specific subset of species *s*, we take a bootstrap sample from the posterior of *B* and *σ*, subset the matrix *B* by taking the rows and colums correspondin to *s*, and calcualte the negative row sum of this submatrix *B*<sub>*s*</sub>, yielding an estimate of *x*<sup>(*s*)</sup>. To estimate the prediction interval, which takes into account the error, we can sample this endpoint abundance from a lognormal distribution, with standard deviation of *σ*<sub>*s*</sub>.
+
+This process is implemented in the `boostrap_results` function, which takes the previous `stan_results` list, along with a specified number of boostrap samples, here set to 500:
+
+``` r
+br <- bootstrap_results(stan_results, nboot=500)
+```
+
+    ## [1] "Bootstrapping community 1 of 15"
+    ## [1] "Bootstrapping community 2 of 15"
+    ## [1] "Bootstrapping community 3 of 15"
+    ## [1] "Bootstrapping community 4 of 15"
+    ## [1] "Bootstrapping community 5 of 15"
+    ## [1] "Bootstrapping community 6 of 15"
+    ## [1] "Bootstrapping community 7 of 15"
+    ## [1] "Bootstrapping community 8 of 15"
+    ## [1] "Bootstrapping community 9 of 15"
+    ## [1] "Bootstrapping community 10 of 15"
+    ## [1] "Bootstrapping community 11 of 15"
+    ## [1] "Bootstrapping community 12 of 15"
+    ## [1] "Bootstrapping community 13 of 15"
+    ## [1] "Bootstrapping community 14 of 15"
+    ## [1] "Bootstrapping community 15 of 15"
+
+From this, we can plot the results using the `plot_boot_results` function, either specifying the observed vs. predicted plot (`show_plot = "obs_pred"`) or the violin plots of the prediction intervals (`show_plot = "violin"`), or both:
+
+``` r
+pl <- plot_boot_results(br, show_plot = "both")
+```
+
+    ## [1] "Plotting violin plots"
+
+![](bayes_files/figure-markdown_github/unnamed-chunk-8-1.png)
+
+    ## [1] "Plotting observed vs. pred"
+
+![](bayes_files/figure-markdown_github/unnamed-chunk-8-2.png)
+
+If no communities were excluded from the fitting process, this function will plot all the posterior prediction interval of all communities (including unobserved communities, for example, `as-la-po`, above).
+
+### Step 4: predicting out-of-fit
+
+We can repeat this above anaalysis, but with the removal of one or more communities to test the out-of-fit predictions. To do this, we specify which communities to exclude by listing as a character vector when we call `fit_stan`. Importantly, this function performs a check on the endpoint matrix: if we exlcude too many communitie, or if the original endpoint matrix contain an insufficient number of endpoints to fit *B*, then this function will return an error before calling Stan. Here, for example, we exclude too many communities, such that speces `as` and `fa` never occur together:
+
+``` r
+stan_reduced <- fit_stan(dt, exclude = c("as-fa","as-fa-la","as-fa-la-po"),
+                         stan_file = "Stan_file.stan", chains = 2, cores = 2, iter = 5000, 
+                         warmup = 2000, thin = 15, seed=12)
+```
+
+    ## [1] "excluding communities as-fa, as-fa-la, as-fa-la-po"
+
+    ## Error in check_endpoints(E): Not enough endpoint to fit
+
+Otherwise, provided there are enough endpoints, the specified communities are removed from the dataset, and the Stan file is implemented on the reduced data:
+
+``` r
+stan_reduced <- fit_stan(dt, exclude = c("po","as-fa-la"),
+                         stan_file = "Stan_file.stan", chains = 2, cores = 2, iter = 5000, 
+                         warmup = 2000, thin = 15, seed=12)
+```
+
+    ## [1] "excluding communities po, as-fa-la"
+
+After bootrapping these results, the `plot_boot_results` function plots the predictions for the ommitted communities:
+
+``` r
+br_reduced <- bootstrap_results(stan_reduced, nboot=500)
+```
+
+    ## [1] "Bootstrapping community 1 of 2"
+    ## [1] "Bootstrapping community 2 of 2"
+
+``` r
+pl_reduced <- plot_boot_results(br_reduced, show_plot = "both")
+```
+
+    ## [1] "Plotting violin plots"
+
+![](bayes_files/figure-markdown_github/unnamed-chunk-11-1.png)
+
+    ## [1] "Plotting observed vs. pred"
+
+![](bayes_files/figure-markdown_github/unnamed-chunk-11-2.png)
--- code/bayesian_approach/bayes.R
+++ code/bayesian_approach/bayes.R
@@ -3,57 +3,49 @@ library(rstan)
 library(coda)
 library(tidyverse)
 
-
-# assign a label to each community based on the presence/absence of each species
-assign_labels <- function(E){
-	# get the species names and num sp
-	sp_names <- sort(names(E))
-	nspp <- length(sp_names)
-	# sort the cols alphabetically
-	E <- E %>% select(sp_names)
- 	# create label names	
-	labels <- expand.grid(replicate(nspp, 0:1, simplify = FALSE)) %>% filter(rowSums(.)>0) %>% as_tibble %>% setNames(sp_names) %>% 
-		mutate(comm_id = as.numeric((.>0)%*%(2^(0:(ncol(.)-1))))) %>% gather(species,abundance,-comm_id) %>% filter(abundance>0) %>% 
-		select(-abundance) %>% group_by(comm_id) %>% summarize(label=paste(species,collapse="-"))
-	# assigne labels and label names
- 	E <- E %>% filter(rowSums(.)>0) %>% mutate(tot_abund=rowSums(.), comm_id = as.numeric((.>0)%*%(2^(0:(ncol(.)-1))))) %>% 
-			arrange(comm_id, tot_abund) %>% select(-tot_abund) %>% left_join(labels,"comm_id")
- 	# rep numbers
- 	rep_ids <- E %>% group_by(comm_id) %>% tally() %>% ungroup %>% group_by(comm_id) %>% expand(count = seq(1:n)) %>% ungroup %>% arrange(comm_id, count) %>% select(-comm_id) %>% rename(rep_num = count)
-	# clean up
- 	E <- E %>% bind_cols(rep_ids) %>% select(-comm_id) %>% rename(comm_id=label)
-	return(E)
+prepare_data <- function(filename = "../../data/Kuebbing_plants/natives.csv"){
+	# Input
+	# filename: file name of a csv file containing the endpoints (rows are observations, columns are species, headers are species names)
+	# Output
+	# a tibble as the csv file but with an extra column containing the label for the community
+	dt <- read_csv(filename) 
+	# now label communities
+	spnames <- colnames(dt)
+	dt <- dt %>% add_column(community = apply(dt, 1, function(x) paste0(spnames[x > 0], collapse = "-")))
+	return(dt)
 }
 
 # estimate B using a Bayesian appraoch via Stan
-fit_stan <- function(E_full, stan_file = "../code/Stan_file.stan", B_upper, B_lower, exclude = NULL, 
-					 chains, cores, iter, thin, warmup, seed=122457){
+fit_stan <- function(dt, stan_file, exclude = NULL, B_upper, B_lower,  
+					 chains, cores, iter, thin, warmup, seed=10){
 	# make sure it has a names() field present
-	if(!is_tibble(E) | !is.data.frame(E)){
+	if(!is_tibble(dt) | !is.data.frame(dt)){
 		stop("E must be a tibble or data frame")
 	}	
-	# assign comm_ids, for outputting
-	E_full <- assign_labels(E)
-	E <- E_full %>% select(-comm_id, -rep_id)
-	# stan options
-	options(mc.cores = parallel::detectCores())	
-	Sys.setenv(USE_CXX14 = 1)	
-	rstan_options(auto_write = TRUE)
-	# make sure the comm labels are in the right order
-	exclude <- as.character(sapply(exclude, function(x) paste(sort(strsplit(x,"-")[[1]]),collapse="-")))
-	if(any(!exclude%in%unique(E_full$comm_id))){
-		stop("`exclude' contains unobserved communities")
+	# strip out the exluded communities if provided
+	if(!is.null(exclude)){
+		# make sure the comm labels are in the right order
+		exclude <- as.character(sapply(exclude, function(x) paste(sort(strsplit(x,"-")[[1]]),collapse="-")))
+		# make sure the excluded communities are labeled right
+		if(any(!exclude%in%unique(dt$community))){
+			stop("`exclude' contains unobserved communities")
+		}
+		else{
+			print(paste("excluding communities",paste(exclude, collapse = ", ")))
+			# remove the excluded communities and the community indicator
+			E <- dt %>% filter(!community%in%exclude) %>% select(-community) %>% as.matrix()
+		}
+	}
+	else{
+		print("fitting all endpoints")
+		E <- dt %>% select(-community) %>% as.matrix()
 	}
 	nspp <- ncol(E)
 	sp_names <- colnames(E)
 	# create indicators for upper/lower bounds
 	if(missing(B_upper)){
-		B_upper <- matrix(1,nspp,nspp)		
-		diag(B_upper) <- 0
-		if("fa"%in%sp_names){
-			# constrain the plant community to be competitive
-			B_upper <- matrix(0,nspp,nspp) 
-		}
+		# constrain the plant community to be competitive
+		B_upper <- matrix(0,nspp,nspp) 
 	}
 	if(missing(B_lower)){
 		B_lower <- matrix(-1,nspp,nspp)
@@ -63,7 +55,7 @@ fit_stan <- function(E_full, stan_file = "../code/Stan_file.stan", B_upper, B_lo
 	# set the s.d. for the prior to be the largest diagonal element
 	maxB <- max(abs(B_init))		
 	# build stan data list
-	stan_data <- list(  N = ncol(E),
+	stan_data <- list(	N = ncol(E),
 						M = nrow(E),
 						E = E,
 						B_upper = B_upper,
@@ -72,70 +64,72 @@ fit_stan <- function(E_full, stan_file = "../code/Stan_file.stan", B_upper, B_lo
 						maxB = maxB)
 	# initialization vector
 	init_list <- replicate(chains,list(sigmax = rep(0.25,nspp), B = B_init), simplify = FALSE)	
+	# stan options
+	Sys.setenv(USE_CXX14 = 1)	
+	rstan_options(auto_write = TRUE)	
+	# check to make sure we have enough endpoints
+	check_endpoints(E)
 	# fit the stan model!
 	stan_fit <- stan(stan_file, data=stan_data, cores = cores, iter=iter, thin = thin, warmup=warmup, 
-					  chains = chains, seed=seed, init=init_list)
-	return(list(E_full = E_full, E = E, stan_fit=stan_fit, exclude = exclude, B_upper = B_upper, B_lower = B_lower, B_init = B_init, seed = seed))
+						chains = chains, seed=seed, init=init_list)
+	return(list(E_full = dt, E = E, stan_fit=stan_fit, exclude = exclude, B_upper = B_upper, B_lower = B_lower, B_init = B_init, seed = seed))
 }
 			
-# estimate B via the naive linear regression approach
-fit_lm <- function(E){
-	# make sure it has a names() field present
-	if(!is_tibble(E) | !is.data.frame(E)){
-		stop("E must be a tibble or data frame")
-	}	
-	# assign comm_ids, for outputting
-	E_full <- assign_labels(E)	
+# make sure we have enough endpoints, using lm. returns an error code if not
+check_endpoints <- function(E){
+	nspp <- ncol(E)
 	# get the median value across 
-	Emean <- E_full %>% gather(species, abundance, -comm_id, -rep_num) %>% filter(abundance>0) %>% group_by(species, comm_id) %>% 
-		summarize(abundance = median(abundance)) %>% spread(species, abundance, fill=0) %>% arrange(desc(comm_id)) %>% select(-comm_id) %>% as.matrix()
-	# get the naive regression estimate of B
+	Emean <- E %>% as_tibble %>% add_column(community = apply(., 1, function(x) paste0(colnames(.)[x > 0], collapse = "-"))) %>% 
+		gather(species, abundance, -community) %>% filter(abundance>0) %>% group_by(species, community) %>% 
+		summarize(abundance = median(abundance)) %>% spread(species, abundance, fill=0) %>% select(-community) %>% as.matrix()
 	B_lm <- matrix(NA,nspp,nspp)
+	# fit each species independently, filling in each row of B
 	for(i in 1:nspp){
 		myE <- Emean[Emean[,i]>0,]
 		B_lm[i,] <- as.numeric(lm(rep(-1,nrow(myE))~-1+myE)$coefficients)
 	}
-	return(B_lm)
+	if(any(is.na(B_lm))){
+		stop("Not enough endpoint to fit")
+	}
+	return()
 }
 	
 # plot the mcmc results, along with histograms and obs vs. pred
-plot_diagnostics <- function(stan_results){
+plot_diagnostics <- function(stan_results, show_plot="both"){
+	if(show_plot == "both"){
+		show_plot <- c("chains","hist")
+	}	
+	if(!any(c("chains","hist")%in%show_plot)){
+		stop("Invalid plot option, 'show_plot' must be 'chains', 'hist', or 'both'")
+	}
 	with(stan_results, {
 		sp_names <- colnames(E)
 		nspp <- length(sp_names)
-		# plot the mcmc runs
-		for(i in 1:length(stan_fit@model_pars)){
-			if(stan_fit@model_pars[i]!="Blim"){
-				post_plot<-As.mcmc.list(stan_fit,pars=stan_fit@model_pars[i]) # this samples from the posterior
-				if(ncol(post_plot[[1]])>1){
-						show(plot(post_plot)) # this shows the convergence
-				}
-				else{
-					show(plot(post_plot,main=stan_fit@model_pars[i]))
+		if("chains"%in%show_plot){
+			print("Plotting MCMC chains")
+			# plot the mcmc runs
+			for(i in 1:length(stan_fit@model_pars)){
+				if(stan_fit@model_pars[i]!="Blim"){
+					post_plot<-As.mcmc.list(stan_fit,pars=stan_fit@model_pars[i]) # this samples from the posterior
+					if(ncol(post_plot[[1]])>1){
+							show(plot(post_plot)) # this shows the convergence
+					}
+					else{
+						show(plot(post_plot,main=stan_fit@model_pars[i]))
+					}
 				}
 			}
 		}
 		# wrap the chains into a matrix
 		stan_fit <- as.matrix(stan_fit)
 		# grab the individual components and rename
-		B_stan <- stan_fit[,1:nspp^2]
-		sigma_stan <- stan_fit[,grepl("sigma",colnames(stan_fit)) | grepl("lp",colnames(stan_fit))]
-		# get the predicted and observed eq abundances, using the median B
-		pred_eq <- NULL
-		my_B <- matrix(apply(B_stan,2,median),nspp,nspp)
-		for(i in 1:nrow(E)){
-			subs <- (1:nspp)[E[i,]>0]
-			pred_x <-  -rowSums(solve(my_B[subs,subs]))
-			if(any(pred_x<0)){
-				pred_x <- rep(NA,length(pred_x))
-			}
-			pred_eq <- rbind(pred_eq, data_frame(Predicted=pred_x,Observed=E[i,subs],species=sp_names[subs],comm_id=as.numeric(matrix((E[i,]>0),nrow=1)%*%(2^(0:(nspp-1)))), rep=i))
+		B_stan <- stan_fit[,1:nspp^2] %>% as_tibble() %>% setNames(apply(expand.grid(sp_names,sp_names),1,function(x) paste(x,collapse="-")))
+		if("hist"%in%show_plot){
+			print("Plotting histogram of B")
+			# plot the histograms 
+			show(ggplot(B_stan %>% gather("B_ij", "value"), aes(x = value)) + geom_histogram() + geom_vline(xintercept=0, col="darkred", linetype=2)+facet_wrap(~B_ij, scales = "free") + 
+						 	theme(axis.text.x = element_text(size=5))+theme_bw())
 		}
-		# plot
-		show(ggplot(pred_eq,aes(x=Observed, y=Predicted,color=species))+geom_point()+scale_y_log10()+scale_x_log10()+geom_abline(intercept=0, slope=1)+ylab("Predicted equilibrium abundances") + xlab("Observed abundances"))
-		# plot the histograms 
-		show(ggplot(B_stan %>% data.frame() %>% gather("B_ij", "value"), aes(x = value)) + geom_histogram() + geom_vline(xintercept=0, col="darkred", linetype=2)+facet_wrap(~B_ij, scales = "free") + 
-					 	theme(axis.text.x = element_text(size=5))+theme_bw())
 	})
 	
 }
@@ -147,12 +141,12 @@ bootstrap_results <- function(stan_results, nboot){
 		sp_names <- colnames(E)
 		# create a matrix of communities presence/absence and label name
 		labels <- expand.grid(replicate(nspp, 0:1, simplify = FALSE)) %>% filter(rowSums(.)>0) %>% as_tibble %>% setNames(sp_names) %>% 
-			mutate(comm_id = as.numeric((.>0)%*%(2^(0:(ncol(.)-1))))) %>% gather(species,abundance,-comm_id) %>% filter(abundance>0) %>% 
-			select(-abundance) %>% group_by(comm_id) %>% summarize(label=paste(species,collapse="-")) %>% left_join(expand.grid(replicate(nspp, 0:1, simplify = FALSE)) %>% filter(rowSums(.)>0) %>% as_tibble %>% setNames(sp_names) %>% 
-			mutate(comm_id = as.numeric((.>0)%*%(2^(0:(ncol(.)-1))))), "comm_id") %>% select(-comm_id) %>% rename(comm_id = label)
+			mutate(community = as.numeric((.>0)%*%(2^(0:(ncol(.)-1))))) %>% gather(species,abundance,-community) %>% filter(abundance>0) %>% 
+			select(-abundance) %>% group_by(community) %>% summarize(label=paste(species,collapse="-")) %>% left_join(expand.grid(replicate(nspp, 0:1, simplify = FALSE)) %>% filter(rowSums(.)>0) %>% as_tibble %>% setNames(sp_names) %>% 
+			mutate(community = as.numeric((.>0)%*%(2^(0:(ncol(.)-1))))), "community") %>% select(-community) %>% rename(community = label)
 		# if doing out of fit, only predict the unobserved
-		if(any(exclude%in%labels$comm_id)){
-			labels <- labels %>% filter(comm_id%in%exclude)
+		if(any(exclude%in%labels$community)){
+			labels <- labels %>% filter(community%in%exclude)
 		}
 		# conver the stan results to a matrix
 		stan_mat <- as.matrix(stan_fit)
@@ -165,8 +159,8 @@ bootstrap_results <- function(stan_results, nboot){
 		for(i in 1:nrow(labels)){
 			print(paste("Bootstrapping community",i,"of", nrow(labels)))
 			# get the current community id and species locations
-			my_id <- labels$comm_id[i]
-			my_sp <- (1:nspp)[labels %>% select(-comm_id) %>% slice(i) %>% unlist %>% as.logical()]
+			my_id <- labels$community[i]
+			my_sp <- (1:nspp)[labels %>% select(-community) %>% slice(i) %>% unlist %>% as.logical()]
 			n_coex <- 0
 			for(j in 1:nboot){
 				# sample an index
@@ -185,10 +179,10 @@ bootstrap_results <- function(stan_results, nboot){
 					n_coex <- n_coex+1
 				}
 				# add to abund_mat
-				abund_mat <- bind_rows(abund_mat,tibble(comm_id = my_id, species = sp_names[my_sp], abundance = my_x, abundance_noise = my_x_noise, coexist = as.numeric(all(my_x>0)), out_fit = as.numeric(my_id%in%exclude)))			
+				abund_mat <- bind_rows(abund_mat,tibble(community = my_id, species = sp_names[my_sp], abundance = my_x, abundance_noise = my_x_noise, coexist = as.numeric(all(my_x>0)), out_fit = as.numeric(my_id%in%exclude)))			
 			}
 			# get the proportion that go extinct
-			coexist_mat <- bind_rows(coexist_mat, tibble(comm_id = my_id, num_coexist = n_coex, num_comms = nboot) %>% mutate(prop_extinct = (num_comms - n_coex)/num_comms))
+			coexist_mat <- bind_rows(coexist_mat, tibble(community = my_id, num_coexist = n_coex, num_comms = nboot) %>% mutate(prop_extinct = (num_comms - n_coex)/num_comms))
 		}
 		return(append(stan_results, list(boot_X = abund_mat, boot_C = coexist_mat)))
 	})
@@ -196,26 +190,40 @@ bootstrap_results <- function(stan_results, nboot){
 
 
 # plot the boostrap results, both violin and obs vs. pred
-plot_boot_results <- function(boot_results){
+plot_boot_results <- function(boot_results, show_plot = "both"){
+	if(show_plot == "both"){
+		show_plot <- c("violin","obs_pred")
+	}
+	if(!any(c("violin","obs_pred")%in%show_plot)){
+		stop("Invalid plot option, 'show_plot' must be 'violin' or 'obs_pred', or 'both'")
+	}	
+	out_list <- NULL
 	with(boot_results, {
-		# relevel the comm_ids by size
-		boot_X <- boot_X %>% mutate(comm_id = factor(comm_id, level=boot_X %>%  select(comm_id) %>% distinct() %>% mutate(len = nchar(comm_id)) %>% arrange(len, comm_id) %>% select(-len) %>% unlist %>% as.character()))
-		# gather the observed datapoints, filter to the current fitted values, and relevel comm_id
-		obs_X_filt <- E_full %>% filter(comm_id%in%unique(boot_X$comm_id)) %>% select(-rep_num)  %>% gather(species, abundance, -comm_id) 
-		obs_X <- obs_X_filt %>% mutate(comm_id = factor(comm_id, level  = E_full %>%  select(comm_id) %>% distinct() %>% mutate(len = nchar(comm_id)) %>%
-				arrange(len, comm_id) %>% select(-len) %>% unlist %>% as.character())) %>% filter(abundance>0)
+		# relevel the communitys by size
+		boot_X <- boot_X %>% mutate(community = factor(community, level=boot_X %>% select(community) %>% distinct() %>% mutate(len = nchar(community)) %>% arrange(len, community) %>% select(-len) %>% unlist %>% as.character()))
+		# gather the observed datapoints, filter to the current fitted values, and relevel community
+		obs_X_filt <- E_full %>% filter(community%in%unique(boot_X$community)) %>% gather(species, abundance, -community) 
+		obs_X <- obs_X_filt %>% mutate(community = factor(community, level	= E_full %>% select(community) %>% distinct() %>% mutate(len = nchar(community)) %>%
+				arrange(len, community) %>% select(-len) %>% unlist %>% as.character())) %>% filter(abundance>0)
 		# plot violins
-		show(g1 <- ggplot(boot_X %>% filter(coexist==1), aes(y=abundance_noise, x= species, fill=species))+geom_violin(alpha=0.8)+
-			geom_point(data=obs_X, aes(x=species, y = abundance), color="black",size=1,shape=5, alpha=0.6) +
-			facet_grid(.~comm_id, scales="free_x", space = "free_x")+scale_y_log10()+theme_bw()+ylab("Abundance"))+
+		g1 <- ggplot(boot_X %>% filter(coexist==1), aes(y=abundance_noise, x= species, fill=species))+geom_violin(alpha=0.8)+
+				geom_point(data=obs_X, aes(x=species, y = abundance), color="black",size=1,shape=5, alpha=0.6) +
+				facet_grid(.~community, scales="free_x", space = "free_x")+scale_y_log10()+theme_bw()+ylab("Abundance")+
 				theme(axis.title.x = element_blank(), legend.position = "none")
 		# get median obs vs. pred values
-		obs_pred <- obs_X_filt %>% group_by(comm_id, species) %>% filter(abundance>0) %>% summarize(Observed = median(abundance)) %>% 
-				left_join(boot_X %>% mutate(comm_id = as.character(comm_id)) %>% filter(coexist==1, abundance>0) %>% group_by(comm_id, species) %>% summarize(Predicted = median(abundance)), c("comm_id","species"))
-		# plot
-		show(g2 <- ggplot(obs_pred, aes(x=Observed, y=Predicted, fill=species)) + geom_point(shape=23, alpha=0.6, size=3) + geom_abline(intercept=0, slope=1)+theme_bw()+scale_y_log10()+scale_x_log10())
-			 
-		return(list(g1 = g1, g2 = g2))
+		obs_pred <- obs_X_filt %>% group_by(community, species) %>% filter(abundance>0) %>% summarize(Observed = median(abundance)) %>% 
+					left_join(boot_X %>% mutate(community = as.character(community)) %>% filter(coexist==1, abundance>0) %>% group_by(community, species) %>% summarize(Predicted = median(abundance)), c("community","species"))
+		g2 <- ggplot(obs_pred, aes(x=Observed, y=Predicted, fill=species)) + geom_point(shape=23, alpha=0.6, size=3) + geom_abline(intercept=0, slope=1)+theme_bw()+scale_y_log10()+scale_x_log10()
+		# show the plots
+		if("violin"%in%show_plot){
+			print("Plotting violin plots")
+			show(g1)
+		}
+		if("obs_pred"%in%show_plot){
+			print("Plotting observed vs. pred")
+			show(g2)
+		}
+		return(list(obs_pred = g2, violin = g1))
 	})
 }
 
